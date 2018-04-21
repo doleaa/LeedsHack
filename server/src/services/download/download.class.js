8938c181f8756f8c7f5905194c1199d882b955af
alex <alexandru.dolea@student.manchester.ac.uk>
2018-04-21 21:17:25 +0100

    Server:
    -fully-implemented download service class

--- server/src/services/download/download.class.js
+++ server/src/services/download/download.class.js
@@ -1,3 +1,5 @@
+const slsk = require('slsk-client')
+
 /* eslint-disable no-unused-vars */
 class Service {
   constructor (options) {
@@ -5,7 +7,43 @@ class Service {
   }
 
   async find (params) {
-    return [];
+    return new Promise((resolve, reject) => {
+      if (!(params.query.usr && params.query.pwd && params.query.bitrate 
+        && params.query.file && params.query.size && params.query.slots 
+        && params.query.speed && params.query.user)) {
+        reject(new Error())
+      }
+  
+      const user = params.query.usr
+      const pass = params.query.pwd
+      let songObj = {}
+      songObj.bitrate = params.query.bitrate
+      songObj.file = params.query.file
+      songObj.size = params.query.size
+      songObj.slots = params.query.slots
+      songObj.speed = params.query.speed
+      songObj.user = params.query.user
+
+      slsk.connect({
+        user,
+        pass
+      }, (error, response) => {
+        if (error) {
+          reject(new Error())
+        } else {
+          const client = response
+          client.download({
+            file: songObj
+          }, (dowloadError, data) => {
+            if (dowloadError) {
+              reject(new Error())
+            } else {
+              resolve(data.buffer)
+            }
+          })
+        }
+      })
+    })
   }
 
   async get (id, params) {
