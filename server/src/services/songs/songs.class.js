e36078edab8aa67dddda5fbceebcef4b106871f1
alex <alexandru.dolea@student.manchester.ac.uk>
2018-04-21 19:42:41 +0100

    Server:
    -fully-implemented songs service class
    -remaining: download

--- server/src/services/songs/songs.class.js
+++ server/src/services/songs/songs.class.js
@@ -1,3 +1,5 @@
+const slsk = require('slsk-client')
+
 /* eslint-disable no-unused-vars */
 class Service {
   constructor (options) {
@@ -9,6 +11,38 @@ class Service {
   }
 
   async get (id, params) {
+    return new Promise((resolve, reject) => {
+      if (!(params.query.usr && params.query.pwd)) {
+        reject(new Error())
+      }
+  
+      const songSearchString = id
+      const user = params.query.usr ? params.query.usr : ""
+      const pass = params.query.pwd ? params.query.pwd : ""
+      
+      slsk.connect({
+        user,
+        pass
+      }, (error, response) => {
+        if (error) {
+          reject(new Error())
+        } else {
+          let client = response
+
+          client.search({
+            req: songSearchString,
+            timeout: 4000
+          }, (clientSearchError, clientSearchResponse) => {
+            if (clientSearchError) {
+              reject(new Error())
+            } else {
+              resolve(clientSearchResponse)
+            }
+          })
+        }
+      })
+    })
+    
     return {
       id, text: `A new message with ID: ${id}!`
     };
